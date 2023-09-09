bf94ff516d96a40ca467115102340ad369b55235
= <=>
2023-09-09 16:59:08 +0200

    start

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
