d117d0a431773a7cd4c4e1667d33f4a18517954a
jorrit09 <501149@vistacollege.nl>
2023-09-13 19:00:22 +0200

    qwd

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
+import AppNavigation from "./navigation/appNavigation";
 
 export default function App() {
-  return (
-   <View className="flex-1 items-center justify-center">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigation />;
 }
